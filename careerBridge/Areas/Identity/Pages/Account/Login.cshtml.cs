aaa28c0a6f184bc4f5ada6805f6f921cd396fc35
Sagar <sarth@LAPTOP-PC47DRG1>
2025-07-12 03:10:51 -0400

    Employer path changed for login

--- careerBridge/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ careerBridge/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -102,7 +102,7 @@ namespace careerBridge.Areas.Identity.Pages.Account
                     }
                     else if (roles.Contains("Employer"))
                     {
-                        return LocalRedirect("~/Home/Employer"); //for now cause we dont have employer dashboard
+                        return LocalRedirect("~/Employer/Index"); //for now cause we dont have employer dashboard
                     }
                 }
                 if (result.RequiresTwoFactor)
