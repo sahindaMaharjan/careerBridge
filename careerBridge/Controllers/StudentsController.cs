a123d17c6224b5412124d9fbdcf593af9c3c41ad
Sahinda Maharjan <sahindamaharjan2016@gmail.com>
2025-07-13 00:46:15 -0400

    jobList

--- careerBridge/Controllers/StudentsController.cs
+++ careerBridge/Controllers/StudentsController.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using careerBridge.Areas.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace careerBridge.Controllers
 {
     public class StudentController : Controller
     {
         // This will pick up Views/Student/Index.cshtml
+        private readonly careerBridgeDb _context;
         public IActionResult Index()
         {
             return View();
+        }
+        public StudentController(careerBridgeDb context)
+        {
+            _context = context;
+        }
+
+        public IActionResult JobList() 
+        {
+            var jobs = _context.JobListings.ToList();
+            return View(jobs);
         }
     }
 }
