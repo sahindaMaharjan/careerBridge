23863c758e7d4b02a07697d14f361ef3e4b49d0b
Swikrit-spec <swikritkhatiwada100@gmail.com>
2025-07-13 11:50:02 -0400

    Misc

--- careerBridge/Controllers/StudentsController.cs
+++ careerBridge/Controllers/StudentsController.cs
@@ -1,28 +1,17 @@
-<<<<<<< HEAD
-﻿using careerBridge.Models;
+using careerBridge.Models;
 using careerBridge.Services;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-=======
-﻿using careerBridge.Areas.Identity.Data;
-using Microsoft.AspNetCore.Mvc;
->>>>>>> a123d17c6224b5412124d9fbdcf593af9c3c41ad
 
 namespace careerBridge.Controllers
 {
     public class StudentController : Controller
     {
-<<<<<<< HEAD
         private readonly JobSearchService _jobSearchService;
 
         public StudentController()
-=======
-        // This will pick up Views/Student/Index.cshtml
-        private readonly careerBridgeDb _context;
-        public IActionResult Index()
->>>>>>> a123d17c6224b5412124d9fbdcf593af9c3c41ad
         {
             _jobSearchService = new JobSearchService();
         }
@@ -47,16 +36,6 @@ namespace careerBridge.Controllers
         {
             TempData["Message"] = $"You applied for: {jobTitle} at {company}";
             return RedirectToAction("Index");
-        }
-        public StudentController(careerBridgeDb context)
-        {
-            _context = context;
-        }
-
-        public IActionResult JobList() 
-        {
-            var jobs = _context.JobListings.ToList();
-            return View(jobs);
         }
     }
 }
--- careerBridge/Services/JobSearchService.cs
+++ careerBridge/Services/JobSearchService.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace careerBridge.Services
@@ -6,7 +7,7 @@ namespace careerBridge.Services
     public class JobSearchService
     {
         private readonly HttpClient _httpClient;
-        private const string apiKey = "5368558acemshdffde0a1cf12249p1d0d9ejsn9fc47e8eb1c3";  // Put your RapidAPI key here
+        private const string apiKey = "5368558acemshdffde0a1cf12249p1d0d9ejsn9fc47e8eb1c3";  // Replace with your actual key
         private const string apiHost = "jsearch.p.rapidapi.com";
         private const string baseUrl = "https://jsearch.p.rapidapi.com/search";
 
@@ -17,49 +18,21 @@ namespace careerBridge.Services
             _httpClient.DefaultRequestHeaders.Add("X-RapidAPI-Host", apiHost);
         }
 
-        public async Task<string> SearchJobsAsync(string query)
+        public async Task<string> SearchJobsAsync(string query, string location = "", int? posted = null, int? minSalary = null)
         {
             var url = $"{baseUrl}?query={Uri.EscapeDataString(query)}";
-            var response = await _httpClient.GetAsync(url); using System.Net.Http;
-            using System.Threading.Tasks;
 
-namespace careerBridge.Services
-    {
-        public class JobSearchService
-        {
-            private readonly HttpClient _httpClient;
-            private const string apiKey = "YOUR_RAPIDAPI_KEY";  // Replace with your actual key
-            private const string apiHost = "jsearch.p.rapidapi.com";
-            private const string baseUrl = "https://jsearch.p.rapidapi.com/search";
+            if (!string.IsNullOrWhiteSpace(location))
+                url += $"&location={Uri.EscapeDataString(location)}";
 
-            public JobSearchService()
-            {
-                _httpClient = new HttpClient();
-                _httpClient.DefaultRequestHeaders.Add("X-RapidAPI-Key", apiKey);
-                _httpClient.DefaultRequestHeaders.Add("X-RapidAPI-Host", apiHost);
-            }
+            if (posted.HasValue)
+                url += $"&date_posted={posted.Value}";
 
-            public async Task<string> SearchJobsAsync(string query, string location = "", int? posted = null, int? minSalary = null)
-            {
-                var url = $"{baseUrl}?query={Uri.EscapeDataString(query)}";
+            if (minSalary.HasValue)
+                url += $"&min_salary={minSalary.Value}";
 
-                if (!string.IsNullOrWhiteSpace(location))
-                    url += $"&location={Uri.EscapeDataString(location)}";
-
-                if (posted.HasValue)
-                    url += $"&date_posted={posted.Value}"; // RapidAPI may use different param, check docs
-
-                if (minSalary.HasValue)
-                    url += $"&min_salary={minSalary.Value}";
-
-                var response = await _httpClient.GetAsync(url);
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadAsStringAsync();
-            }
-        }
-    }
-
-    response.EnsureSuccessStatusCode();
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsStringAsync();
         }
     }
